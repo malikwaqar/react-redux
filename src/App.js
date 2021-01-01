a5b4ff2bf0c6e3e3cad13b82072b5804fa5983fa
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2021-01-02 03:55:55 +0500

    REDUX LEARNING

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, signin } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>REDUX</h1>
+      <h2>Counter: {counter}</h2>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <br />
+      {isLogged ? (
+        <button onClick={() => dispatch(signin())}>SIGN OUT</button>
+      ) : (
+        <button onClick={() => dispatch(signin())}>SIGN IN</button>
+      )}
+
+      <br />
+      {isLogged ? <h3>You are Logged In</h3> : <h3>You are not Logged In</h3>}
     </div>
   );
 }
